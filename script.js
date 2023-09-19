b57643bce4283e54c9fa1c82217658de74b8ff18
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-09-19 15:36:57 +0100

    added

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ function adsToggle () {
     const contentDiv = document.getElementById("Contents");
     const hidden = document.getElementById("hidden")
     if (contentDiv.style.display === "none") {
-        hidden.innerHTML='Show Add ons'
+        hidden.innerHTML='hide Add-ons'
         contentDiv.style.display = "block";
-    } else {        
-        hidden.innerHTML='Hide Add ons'
+    }else if(contentDiv.style.display = "block"){
+        hidden.innerHTML='show Add-ons'
         contentDiv.style.display = "none";
     }
+        if(caret.classList.contains("deg135")){
+            caret.classList.remove("deg135");
+            caret.classList.add("deg315");
+        }else if(caret.classList.contains("deg315")){
+            caret.classList.remove("deg315");
+            caret.classList.add("deg135");
+        }
 }
 
